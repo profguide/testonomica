a7ba8ec57d2e8792058886191c6a4dccc58334b5
adavydov <david.tema@yandex.ru>
2021-09-12 02:31:39 +0300

    ix MigrateOldPaymentsCommand

--- src/Command/MigrateOldPaymentsCommand.php
+++ src/Command/MigrateOldPaymentsCommand.php
@@ -31,33 +31,33 @@ class MigrateOldPaymentsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // array('id' => '1','provider_user_id' => '1','provider_user_email' => 'asd@asd.ru','token' => '7aeb1d17f4f74b71bb85ec2b0f233696','created_at' => '2020-07-23 20:36:58.875789','payed_at' => '2020-07-23 20:36:58.875505','sum' => '500.00','provider_id' => '1','quiz_id' => '4','is_applied' => '0','is_test' => '0'),
-        $data = include('api_providerpayment.php');
-        foreach ($data as $row) {
-            if (empty($row['payed_at'])) {
-                continue;
-            }
-            $dateTime = \DateTime::createFromFormat('Y-m-d H:i:s.u', $row['created_at'])->format('Y-m-d H:i:s');
-            // payment - id
-            $this->em->getConnection()->insert('payment', [
-                'id' => $row['id'],
-                'service_id' => 1,
-                'sum' => (int)$row['sum'],
-                'created_at' => $dateTime
-            ]);
-            $this->em->getConnection()->insert('provider_payment', [
-                'payment_id' => $row['id'],
-                'provider_id' => $row['provider_id'],
-                'token' => Uuid::v4(),
-                'user' => $row['provider_user_id'],
-            ]);
-            $this->em->getConnection()->insert('payment_status', [
-                'payment_id' => $row['id'],
-                'status' => PaymentStatus::STATUS_EXECUTED,
-                'created_at' => $dateTime,
-            ]);
-            echo "save\n";
-        }
+//        // array('id' => '1','provider_user_id' => '1','provider_user_email' => 'asd@asd.ru','token' => '7aeb1d17f4f74b71bb85ec2b0f233696','created_at' => '2020-07-23 20:36:58.875789','payed_at' => '2020-07-23 20:36:58.875505','sum' => '500.00','provider_id' => '1','quiz_id' => '4','is_applied' => '0','is_test' => '0'),
+//        $data = include('api_providerpayment.php');
+//        foreach ($data as $row) {
+//            if (empty($row['payed_at'])) {
+//                continue;
+//            }
+//            $dateTime = \DateTime::createFromFormat('Y-m-d H:i:s.u', $row['created_at'])->format('Y-m-d H:i:s');
+//            // payment - id
+//            $this->em->getConnection()->insert('payment', [
+//                'id' => $row['id'],
+//                'service_id' => 1,
+//                'sum' => (int)$row['sum'],
+//                'created_at' => $dateTime
+//            ]);
+//            $this->em->getConnection()->insert('provider_payment', [
+//                'payment_id' => $row['id'],
+//                'provider_id' => $row['provider_id'],
+//                'token' => Uuid::v4(),
+//                'user' => $row['provider_user_id'],
+//            ]);
+//            $this->em->getConnection()->insert('payment_status', [
+//                'payment_id' => $row['id'],
+//                'status' => PaymentStatus::STATUS_EXECUTED,
+//                'created_at' => $dateTime,
+//            ]);
+//            echo "save\n";
+//        }
 //        $query = $this->em->createNativeQuery('insert into payment (id, service_id, sum, created_at)
 //            values (1, 1, 500, 2020-12-12)');
 //        $query->execute();
