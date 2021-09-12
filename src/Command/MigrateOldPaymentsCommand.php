e314f93fc6dbb52b8b8760c5e4f75539cf6be0cf
adavydov <david.tema@yandex.ru>
2021-09-12 03:26:49 +0300

    fix Robokassa

--- src/Command/MigrateOldPaymentsCommand.php
+++ src/Command/MigrateOldPaymentsCommand.php
@@ -31,8 +31,9 @@ class MigrateOldPaymentsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        echo 'came: 257C1E4F9D6942D666D512F701F609DA' . PHP_EOL;
-        echo 'gen: '. md5('1:11383:uVtNe469XGI6yB6IKIKs') . PHP_EOL;
+        echo 'came: F5A302C2DE11ABEC9B96FFEFA93440E1' . PHP_EOL;
+        echo 'wrong: e75b557dea1e284209e54e9760e43382' . PHP_EOL;
+        echo 'gen: '. md5('1:11384:CR33GHiwwaD5MD22xpag') . PHP_EOL;
         return Command::SUCCESS;
     }
 
