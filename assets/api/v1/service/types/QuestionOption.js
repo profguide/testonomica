dbb3417b8dec20478f6d8e5e6b61046b21a13b50
adavydov <david.tema@yandex.ru>
2022-01-31 12:06:50 +0300

    testonomica api as dependency

--- assets/api/v1/service/types/QuestionOption.js
+++
@@ -1,7 +0,0 @@
-export default class QuestionOption {
-    constructor(props) {
-        this.value = props.value;
-        this.text = props.text;
-        this.img = props.img;
-    }
-}
--- assets/site-api.js
+++ assets/site-api.js
@@ -1,12 +1,5 @@
-// High-level API main file: welcome screen, quiz screen, result screen, managing acts user does.
-
-import ReactDOM from "react-dom";
-import React from 'react';
-import App from "./api/v1/site/App";
-import {HOST} from "./api/v1/const";
-import ServiceApi from "./api/v1/service/ServiceApi";
-import TncEventDispatcher from "./api/common/TncEventDispatcher";
-import './api/v1/style.scss'
+import Testonomica from 'testonomica_api/src/Testonomica';
+import 'testonomica_api/src/style.scss';
 
 const INIT_AUTO = 'auto';
 const INIT_MANUAL = 'manual';
@@ -16,39 +9,11 @@ const testId = tag.getAttribute('data-test');
 if (!testId) {
     throw new Error('tag must include attribute: data-test');
 }
-const host = tag.getAttribute('data-host') ?? HOST;
+const host = tag.getAttribute('data-host') ?? 'https://testonomica.com';
 const token = tag.getAttribute('data-token') ?? null;
 const init = tag.getAttribute('data-init') ?? null;
 
-class Testonomica {
-    constructor() {
-        this.dispatcher = new TncEventDispatcher();
-    }
-
-    createApp() {
-        const api = new ServiceApi({testId, host, token});
-        ReactDOM.render(<App api={api} dispatcher={this.dispatcher}/>, tag);
-    }
-
-    addEventListener(name, callback) {
-        this.dispatcher.addEventListener(name, callback);
-    }
-}
-
-window.testonomica = new Testonomica();
+window.testonomica = new Testonomica(testId, host, token);
 if (!init || init === INIT_AUTO) {
-    window.testonomica.createApp();
+    window.testonomica.createApp(tag);
 }
-
-// Iframe mode: notify parent about resize
-// const resize_ob = new ResizeObserver(function (entries) {
-//     let rect = entries[0].contentRect;
-//     let height = rect.height;
-//     console.log(height);
-//     window.parent.postMessage({frameHeight: height}, host);
-// });
-// resize_ob.observe(tag);
-// how to use:
-// var api = window.testonomica;
-// api.addEventListener('finish', (data) => {alert(data.key)})
-// api.createApp();
