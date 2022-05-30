ce4d4d9505ec74ed3c51bc099eef6dc2fc5a9f41
adavydov <david.tema@yandex.ru>
2022-05-30 21:56:25 +0300

    refactoring frontend, rebuild frontend 1.0.10

--- assets/test.js
+++ assets/test.js
@@ -1,57 +1,25 @@
-import Testonomica from 'testonomica_api/src/index';
-import Config from 'testonomica_api/src/config';
-import {checkCookie} from './js/fnc';
-import {EVENT_FINISH, EVENT_LOADED} from 'testonomica_api/src/events';
+import {isCookieEnabled, parseConfigFromTag, Testonomica} from 'testonomica_api/src/index';
+import {INIT_AUTO} from 'testonomica_api/src/const'
+import ProgressStorage from "testonomica_api/src/service/storage/ProgressStorage";
+import ProgressFirebaseStorage from "testonomica_api/src/service/storage/ProgressFirebaseStorage";
 import 'testonomica_api/src/style.scss';
-import ProgressStorage from "../../testonomica_api/src/service/storage/ProgressStorage";
-import ProgressFirebaseStorage from "../../testonomica_api/src/service/storage/ProgressFirebaseStorage";
-
-// just for debugging
-window.parent.postMessage({name: 'connect'}, '*');
-
-const INIT_AUTO = 'auto';
-const INIT_MANUAL = 'manual';
 
 const tag = document.getElementById('testonomica_app');
-const testId = tag.getAttribute('data-test');
-if (!testId) {
-    throw new Error('tag must include attribute: data-test');
-}
-const host = tag.getAttribute('data-host') ?? 'https://testonomica.com';
-const token = tag.getAttribute('data-token') ?? null;
-const init = tag.getAttribute('data-init') ?? null;
-
-// настройки теста
-const config = new Config({
-    // показ отчёта
-    displayReport: tag.getAttribute('data-display-report') ?? true,
-    // показ отчёта после загрузки теста (если был)
-    showResultAfterLoad: tag.getAttribute('data-show-result-after-load') ?? true
-});
+const config = parseConfigFromTag(tag);
 
 let storage = null;
-if (checkCookie()) {
-    storage = new ProgressStorage(testId);
-    console.log('Cookie enabled, use localStorage.')
+if (isCookieEnabled()) {
+    storage = new ProgressStorage(config.getTestId());
 } else {
     /** sid will be generated and saved in the localStorage and used if there is no sid in the tag */
-    const sid = tag.getAttribute('data-sid'); // ?? session();
+    const sid = tag.getAttribute('data-sid');
     if (!sid) {
         throw Error('Error: sid has to be specified.');
     }
-    storage = new ProgressFirebaseStorage(testId, sid);
-    console.log('Cookie disabled, use firebase.')
+    storage = new ProgressFirebaseStorage(config.getTestId(), sid);
 }
-window.testonomica = new Testonomica(storage, testId, host, token);
 
-// получен результат в ходе прохождения теста
-window.testonomica.addEventListener(EVENT_FINISH, function (e) {
-    window.parent.postMessage({name: EVENT_FINISH, key: e.key}, '*');
-});
-window.testonomica.addEventListener(EVENT_LOADED, function (e) {
-    window.parent.postMessage({name: EVENT_LOADED}, '*');
-});
-
-if (!init || init === INIT_AUTO) {
+window.testonomica = new Testonomica(storage, config.getTestId(), config.getHost(), config.getToken());
+if (config.getInit() === INIT_AUTO) {
     window.testonomica.createApp(tag, config);
 }
