6c962b740706bb53734a3bc42b2395fc45546513
adavydov <david.tema@yandex.ru>
2023-08-12 16:21:29 +0300

    update recipe doctrine

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,17 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    /**
-     * @Route("/", name="main")
-     * @codeCoverageIgnore
-     */
-    public function index(Request $request): Response
+    #[Route('/', name: 'main')]
+    public function index(): Response
     {
         return $this->render('main/index.html.twig', []);
     }
