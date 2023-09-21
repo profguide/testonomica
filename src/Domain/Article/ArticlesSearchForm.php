71249911501964fc99345597773f1c1e55be4161
adavydov <david.tema@yandex.ru>
2023-09-21 12:04:15 +0200

    articles: lang predicate, add breadcrumbs

--- src/Domain/Article/ArticlesSearchForm.php
+++ src/Domain/Article/ArticlesSearchForm.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Domain\Article;
 
 use App\Entity\ArticleCatalog;
+use App\Subscriber\Locale;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Knp\Component\Pager\PaginatorInterface;
@@ -12,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 final class ArticlesSearchForm
 {
-    public function __construct(private EntityManagerInterface $em, private PaginatorInterface $paginator)
+    public function __construct(private EntityManagerInterface $em, private PaginatorInterface $paginator, private Locale $locale)
     {
     }
 
@@ -24,6 +25,11 @@ final class ArticlesSearchForm
         if ($catalog) {
             $builder->andWhere()->where('a.catalog=:catalog');
             $builder->setParameter(':catalog', $catalog);
+        }
+        if ($this->locale->getValue() === 'en') {
+            $builder->andWhere()->where('a.activeEn > 0');
+        } else {
+            $builder->andWhere()->where('a.active > 0');
         }
         $builder->orderBy('a.id', 'DESC');
 
